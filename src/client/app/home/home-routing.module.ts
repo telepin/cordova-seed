35deb57ed01f0bfc6a4d257187ada67596a2ed9b
Armin Zolfaghari <armin.zolfaghari@telepin.com>
2018-07-11 14:34:52 -0400

    Fix issue with no routes matched on startup on mobile app

--- src/client/app/home/home-routing.module.ts
+++ src/client/app/home/home-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home.component';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: '**', redirectTo: '/' }
     ])
   ],
   exports: [RouterModule]
